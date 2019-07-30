016c1abebb07605e3fdf53e4fc211af3774f6e42
cdrosales <cara10denise@hotmail.com>
2019-07-30 11:25:13 -0400

    fix: fixed the whitespace for results when nothing is selected

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 $(function () {
 
+
+
+
     $(".menu").on("change", function(event){
         let $selection = $('.menu').val()
         console.log($selection)
@@ -9,20 +12,29 @@ $(function () {
         .done(function(data) {
           console.log(data.results)
 
-        $(".item1").append(`<img src="${data.results[0].multimedia[3].url}"><p>${data.results[0].multimedia[3].caption}</p>`);
-        $(".item2").append(`<img src="${data.results[1].multimedia[3].url}"><p>${data.results[1].multimedia[3].caption}</p>`);
-        $(".item3").append(`<img src="${data.results[2].multimedia[3].url}"><p>${data.results[2].multimedia[3].caption}</p>`);
-        $(".item4").append(`<img src="${data.results[3].multimedia[3].url}"><p>${data.results[3].multimedia[3].caption}</p>`);
-        $(".item5").append(`<img src="${data.results[4].multimedia[3].url}"><p>${data.results[4].multimedia[3].caption}</p>`);
-        $(".item6").append(`<img src="${data.results[5].multimedia[3].url}"><p>${data.results[5].multimedia[3].caption}</p>`);
-        $(".item7").append(`<img src="${data.results[6].multimedia[3].url}"><p>${data.results[6].multimedia[3].caption}</p>`);
-        $(".item8").append(`<img src="${data.results[7].multimedia[3].url}"><p>${data.results[7].multimedia[3].caption}</p>`);
-        $(".item9").append(`<img src="${data.results[8].multimedia[3].url}"><p>${data.results[8].multimedia[3].caption}</p>`);
-        $(".item10").append(`<img src="${data.results[9].multimedia[3].url}"><p>${data.results[9].multimedia[3].caption}</p>`);
-        $(".item11").append(`<img src="${data.results[10].multimedia[3].url}"><p>${data.results[10].multimedia[3].caption}</p>`);
-        $(".item12").append(`<img src="${data.results[11].multimedia[3].url}"><p>${data.results[11].multimedia[3].caption}</p>`);
+        let $newsC = $(".newsContainer")
+        console.log($newsC)
+      
+        $newsC.hide()
 
-        $(".menu").reset();
+        $newsC.toggle('fast').show()
+
+
+        $(".item1").append(`<img src="${data.results[0].multimedia[4].url}"><p>${data.results[0].multimedia[4].caption}</p>`);
+        $(".item2").append(`<img src="${data.results[1].multimedia[4].url}"><p>${data.results[1].multimedia[4].caption}</p>`);
+        $(".item3").append(`<img src="${data.results[2].multimedia[4].url}"><p>${data.results[2].multimedia[4].caption}</p>`);
+        $(".item4").append(`<img src="${data.results[3].multimedia[4].url}"><p>${data.results[3].multimedia[4].caption}</p>`);
+        $(".item5").append(`<img src="${data.results[4].multimedia[4].url}"><p>${data.results[4].multimedia[4].caption}</p>`);
+        $(".item6").append(`<img src="${data.results[5].multimedia[4].url}"><p>${data.results[5].multimedia[4].caption}</p>`);
+        $(".item7").append(`<img src="${data.results[6].multimedia[4].url}"><p>${data.results[6].multimedia[4].caption}</p>`);
+        $(".item8").append(`<img src="${data.results[7].multimedia[4].url}"><p>${data.results[7].multimedia[4].caption}</p>`);
+        $(".item9").append(`<img src="${data.results[8].multimedia[4].url}"><p>${data.results[8].multimedia[4].caption}</p>`);
+        $(".item10").append(`<img src="${data.results[9].multimedia[4].url}"><p>${data.results[9].multimedia[4].caption}</p>`);
+        $(".item11").append(`<img src="${data.results[10].multimedia[4].url}"><p>${data.results[10].multimedia[4].caption}</p>`);
+        $(".item12").append(`<img src="${data.results[11].multimedia[4].url}"><p>${data.results[11].multimedia[4].caption}</p>`);
+
+
+        // $(".menu").reset();
     })
 
              .fail(function(){
